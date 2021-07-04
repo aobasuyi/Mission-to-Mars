597a08e6e1500f0073479b141b83ad44a84b47b9
Azk <patosas477@gmail.com>
2021-07-04 15:31:26 -0400

    scraping, app, Resources added

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, url_for
 from flask_pymongo import PyMongo
+import scraping
 
 app = Flask(__name__)
 
@@ -15,8 +16,8 @@ def index():
 @app.route("/scrape")
 def scrape():
    mars = mongo.db.mars
-   #mars_data = scraping.scrape_all()
-   #mars.update({}, mars_data, upsert=True)
+   mars_data = scraping.scrape_all()
+   mars.update({}, mars_data, upsert=True)
    return redirect('/', code=302)
 
 if __name__ == "__main__":
--- scraping.py
+++ scraping.py
@@ -12,7 +12,7 @@ def scrape_all():
     browser = Browser('chrome', **executable_path, headless=True)
 
     news_title, news_paragraph = mars_news(browser)
-
+    
     # Run all scraping functions and store results in a dictionary
     data = {
         "news_title": news_title,
@@ -20,7 +20,8 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "hemispheres": hemispheres (browser)
+        "hemispheres": hemispheres (browser),
+        
 
     }
 
@@ -127,7 +128,7 @@ def hemispheres(browser):
             hemispheres['title'] = titles
             hemispshere_image_urls.append(hemispheres)
         
-        browser.back()
+            browser.back()
 
     return hemispshere_image_urls
 
